3fa62a19487e7a435d69a6ece3da50b2c8fc32b4
Yanni Fraga <yanni_fernanda@hotmail.com>
2019-06-12 13:06:58 -0300

    Front end

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Routes from './routes';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Routes />
+    </React.Fragment>
+  );
+}
+
+export default App;
